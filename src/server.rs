bb027cc54c1a2d3eb3785070141e0bacd9480159
hubert <hubert@hubert.com>
2024-02-13 19:55:28 +0100

    save

--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,9 @@
-use std::{collections::HashMap, convert::Infallible};
+use std::{collections::HashMap, convert::Infallible, net::SocketAddr};
 
 use http_body_util::Full;
 use hyper::{
     body::{Bytes, Incoming},
-    header::{HOST, TRANSFER_ENCODING},
+    header::{HeaderValue, HOST, TRANSFER_ENCODING},
     Request, Response, StatusCode,
 };
 use tokio::process::Command;
@@ -28,7 +28,11 @@ struct Script {
 }
 
 impl Script {
-    async fn server(&self, req: Request<Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    pub async fn server(
+        &self,
+        req: Request<Incoming>,
+        remote: &SocketAddr,
+    ) -> Result<Response<Full<Bytes>>, Infallible> {
         let root = if self.root == "" { "/" } else { &self.root };
 
         if let Some(encoding) = req.headers().get(TRANSFER_ENCODING) {
@@ -50,14 +54,21 @@ impl Script {
             req_path
         };
 
-        let port = req.uri().port_u16().unwrap_or(80);
-
         let mut env: HashMap<String, String> = HashMap::new();
         env.insert("SERVER_SOFTWARE".to_string(), "cgi-server-rs".to_string());
         env.insert("SERVER_PROTOCOL".to_string(), "HTTP/1.1".to_string());
         env.insert("GATEWAY_INTERFACE".to_string(), "CGI/1.1".to_string());
-        if let Some(host) = req.uri().host() {
-            env.insert("HTTP_HOST".to_string(), host.to_string());
+        if let Some(host) = req.headers().get(HOST) {
+            if let Ok(host) = host.to_str() {
+                env.insert("HTTP_HOST".to_string(), host.to_string());
+                if let Some((hostname, port)) = getHostPort(host) {
+                    env.insert("SERVER_NAME".to_string(), hostname.to_string());
+                    env.insert("SERVER_PORT".to_string(), port.to_string());
+                } else {
+                    env.insert("SERVER_NAME".to_string(), host.to_string());
+                    env.insert("SERVER_PORT".to_string(), "80".to_string()); // à revoir
+                }
+            }
         }
         env.insert("REQUEST_METHOD".to_string(), req.method().to_string());
         if let Some(query) = req.uri().query() {
@@ -69,10 +80,33 @@ impl Script {
         env.insert("PATH_INFO".to_string(), path_info.to_string());
         env.insert("SCRIPT_NAME".to_string(), root.to_string());
         env.insert("SCRIPT_FILENAME".to_string(), self.path.clone());
-        env.insert("SERVER_PORT".to_string(), port.to_string());
+
+        env.insert("REMOTE_ADDR".to_string(), remote.ip().to_string());
+        env.insert("REMOTE_HOST".to_string(), remote.ip().to_string());
+        env.insert("REMOTE_PORT".to_string(), remote.port().to_string());
+
+        for k in req.headers().keys() {
+            let k = k.as_str().to_uppercase();
+            let join_str: &str;
+            if k == "COOKIE" {
+                join_str = ";";
+            } else {
+                join_str = ",";
+            }
+            join
+        }
 
         Command::new("echo").arg("coucou").envs(env);
 
         todo!()
+    }
+}
+
+fn getHostPort(value: &str) -> Option<(&str, u16)> {
+    let split: Vec<&str> = value.split(":").collect();
+    if split.len() == 2 {
+        Some((split.get(0)?, split.get(1)?.parse().ok()?))
+    } else {
+        None
     }
 }
