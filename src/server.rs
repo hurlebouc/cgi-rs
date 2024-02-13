85523f7ab3fe9b16c09b9d3d465680571ac85cc4
hubert <hubert@hubert.com>
2024-02-13 07:00:24 +0100

    save

--- src/server.rs
+++ src/server.rs
@@ -1,16 +1,27 @@
-use std::path::PathBuf;
+use std::{
+    convert::Infallible,
+    path::{Path, PathBuf},
+};
+
+use http_body_util::Full;
+use hyper::{
+    body::{Bytes, Incoming},
+    header::TRANSFER_ENCODING,
+    Request, Response, StatusCode,
+};
+use regex::Regex;
 
 struct Script {
     // Path to the CGI executable
-    path: PathBuf,
+    path: String,
 
     // URI, empty for "/"
-    root: PathBuf,
+    root: String,
 
     // Working directory of the CGI executable.
     // If None, base directory of path is used.
     // If path as no base directory, dir is used
-    dir: Option<PathBuf>,
+    dir: Option<String>,
 
     // Environment variables
     env: Vec<(String, String)>,
@@ -20,5 +31,30 @@ struct Script {
 }
 
 impl Script {
-    fn server(&self) {}
+    async fn server(&self, req: Request<Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+        let root = if self.root == "" { "/" } else { &self.root };
+
+        if let Some(encoding) = req.headers().get(TRANSFER_ENCODING) {
+            if encoding == "chunked" {
+                let response = Response::builder()
+                    .status(StatusCode::BAD_REQUEST)
+                    .body(Full::new(Bytes::from(
+                        "Chunked encoding is not supported by CGI.",
+                    )))
+                    .unwrap();
+                return Ok(response);
+            }
+        }
+
+        let req_path = req.uri().path();
+        let path_info = if root != "/" && req_path.starts_with(root) {
+            &req_path[root.len()..]
+        } else {
+            req_path
+        };
+
+        let re = Regex::new(r"Hello (?<name>\w+)!").unwrap();
+
+        todo!()
+    }
 }
