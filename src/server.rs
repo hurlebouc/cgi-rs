bfad08ecd5b94d5c2178d97fa3b8a057e131356f
hubert <hubert@hubert.com>
2024-02-13 13:23:31 +0100

    save

--- src/server.rs
+++ src/server.rs
@@ -1,15 +1,12 @@
-use std::{
-    convert::Infallible,
-    path::{Path, PathBuf},
-};
+use std::{collections::HashMap, convert::Infallible};
 
 use http_body_util::Full;
 use hyper::{
     body::{Bytes, Incoming},
-    header::TRANSFER_ENCODING,
+    header::{HOST, TRANSFER_ENCODING},
     Request, Response, StatusCode,
 };
-use regex::Regex;
+use tokio::process::Command;
 
 struct Script {
     // Path to the CGI executable
@@ -53,7 +50,28 @@ impl Script {
             req_path
         };
 
-        let re = Regex::new(r"Hello (?<name>\w+)!").unwrap();
+        let port = req.uri().port_u16().unwrap_or(80);
+
+        let mut env: HashMap<String, String> = HashMap::new();
+        env.insert("SERVER_SOFTWARE".to_string(), "cgi-server-rs".to_string());
+        env.insert("SERVER_PROTOCOL".to_string(), "HTTP/1.1".to_string());
+        env.insert("GATEWAY_INTERFACE".to_string(), "CGI/1.1".to_string());
+        if let Some(host) = req.uri().host() {
+            env.insert("HTTP_HOST".to_string(), host.to_string());
+        }
+        env.insert("REQUEST_METHOD".to_string(), req.method().to_string());
+        if let Some(query) = req.uri().query() {
+            env.insert("QUERY_STRING".to_string(), query.to_string());
+        }
+        if let Some(path_and_query) = req.uri().path_and_query() {
+            env.insert("REQUEST_URI".to_string(), path_and_query.to_string());
+        }
+        env.insert("PATH_INFO".to_string(), path_info.to_string());
+        env.insert("SCRIPT_NAME".to_string(), root.to_string());
+        env.insert("SCRIPT_FILENAME".to_string(), self.path.clone());
+        env.insert("SERVER_PORT".to_string(), port.to_string());
+
+        Command::new("echo").arg("coucou").envs(env);
 
         todo!()
     }
