12f0a2f0b530b9a9cf0e4d147c98d9d1e0330b39
hubert <hubert@hubert.com>
2024-03-15 13:52:15 +0100

    save

--- src/limit.rs
+++ src/limit.rs
@@ -1,9 +1,13 @@
 use std::{
     pin::Pin,
-    task::{Context, Poll},
+    task::{ready, Context, Poll},
 };
 
-use hyper::body::{Body, Frame};
+use core::future::Future;
+use hyper::{
+    body::{Body, Frame, Incoming},
+    Request, Response,
+};
 use pin_project::pin_project;
 use tokio::sync::OwnedSemaphorePermit;
 use tokio_util::sync::PollSemaphore;
@@ -46,8 +50,8 @@ impl<B: Body> Body for PermittedBody<B> {
     }
 }
 
-pub struct HttpConcurrencyLimit<T> {
-    inner: T,
+pub struct HttpConcurrencyLimit<S> {
+    service: S,
     semaphore: PollSemaphore,
     /// The currently acquired semaphore permit, if there is sufficient
     /// concurrency to send a new request.
@@ -55,4 +59,73 @@ pub struct HttpConcurrencyLimit<T> {
     /// The permit is acquired in `poll_ready`, and taken in `call` when sending
     /// a new request.
     permit: Option<OwnedSemaphorePermit>,
+}
+
+impl<S, B> tower::Service<Request<Incoming>> for HttpConcurrencyLimit<S>
+where
+    S: tower::Service<Request<Incoming>, Response = Response<B>>,
+{
+    type Response = Response<PermittedBody<B>>;
+
+    type Error = S::Error;
+
+    type Future = HttpConcurrencyLimitFut<S::Future>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        // If we haven't already acquired a permit from the semaphore, try to
+        // acquire one first.
+        if self.permit.is_none() {
+            self.permit = ready!(self.semaphore.poll_acquire(cx));
+            debug_assert!(
+                self.permit.is_some(),
+                "ConcurrencyLimit semaphore is never closed, so `poll_acquire` \
+                 should never fail",
+            );
+        }
+
+        // Once we've acquired a permit (or if we already had one), poll the
+        // inner service.
+        self.service.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: Request<Incoming>) -> Self::Future {
+        // Take the permit
+        let permit = self
+            .permit
+            .take()
+            .expect("max requests in-flight; poll_ready must be called first");
+
+        // Call the inner service
+        let future = self.service.call(req);
+
+        HttpConcurrencyLimitFut {
+            future,
+            permit: Some(permit),
+        }
+    }
+}
+
+#[pin_project]
+pub struct HttpConcurrencyLimitFut<F> {
+    #[pin]
+    future: F,
+    permit: Option<OwnedSemaphorePermit>,
+}
+
+impl<F, B, E> Future for HttpConcurrencyLimitFut<F>
+where
+    F: Future<Output = Result<Response<B>, E>>,
+{
+    type Output = Result<Response<PermittedBody<B>>, E>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match self.as_mut().project().future.poll(cx) {
+            Poll::Ready(Ok(resp)) => match self.as_mut().project().permit.take() {
+                Some(permit) => Poll::Ready(Ok(resp.map(|body| PermittedBody::new(permit, body)))),
+                None => unreachable!("Future should not be polled twice"),
+            },
+            Poll::Ready(Err(err)) => Poll::Ready(Err(err)),
+            Poll::Pending => Poll::Pending,
+        }
+    }
 }
